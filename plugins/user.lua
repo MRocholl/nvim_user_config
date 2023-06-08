2202c9cc843537943a7e3c46d104de587139da45
MRocholl <moritz.rocholl@gmail.com>
2023-06-08 22:42:54 +0200

    add plugin for TODO comments and add community plugin for typescript. Also change colorscheme

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,15 @@
+--- TODO: Test
 return {
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
@@ -9,4 +20,5 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  --
 }
