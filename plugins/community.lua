2a4a427d993ab1dd3fa58b6bc5afd2a0e180fe4c
Moritz Rocholl <moritz.rocholl@grimm-water-solutions.de>
2023-08-13 21:28:58 +0200

    add venv selector and astrocommunity pacakges for python and typescript

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,8 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.typescript" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.pack.typescript" },
